44152469a572493975da1ecd3a235aa7d580de1a
Matt Johnson <fireye14@gmail.com>
2020-02-16 22:14:22 -0600

    Fix icon and app name label

--- client/DominosPizzaPicker.Client/DominosPizzaPicker.Client.Android/MainActivity.cs
+++ client/DominosPizzaPicker.Client/DominosPizzaPicker.Client.Android/MainActivity.cs
@@ -12,7 +12,7 @@ using Microsoft.WindowsAzure.MobileServices;
 
 namespace DominosPizzaPicker.Client.Droid
 {
-    [Activity(Label = "DominosPizzaPicker.Client", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Domino's Pizza Picker", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -30,8 +30,8 @@ namespace DominosPizzaPicker.Client.Droid
 
             // Set local db if in emulator
             var f = Build.Fingerprint;
-            Constants.ApplicationURL = f.Contains("vbox") || f.Contains("generic") ? @"http://10.0.2.2/DominosPizzaPicker/" : @"https://dominospizzapicker.azurewebsites.net";
-            //Constants.ApplicationURL = f.Contains("vbox") || f.Contains("generic") ? @"https://dominospizzapicker.azurewebsites.net" : @"https://dominospizzapicker.azurewebsites.net";
+            //Constants.ApplicationURL = f.Contains("vbox") || f.Contains("generic") ? @"http://10.0.2.2/DominosPizzaPicker/" : @"https://dominospizzapicker.azurewebsites.net";
+            Constants.ApplicationURL = f.Contains("vbox") || f.Contains("generic") ? @"https://dominospizzapicker.azurewebsites.net" : @"https://dominospizzapicker.azurewebsites.net";
 
             // Load the main application
             LoadApplication(new App());
