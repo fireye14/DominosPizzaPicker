a71d3184dd3dc2ecac63097e3c7a53b302e0efb4
Matt Johnson <fireye14@gmail.com>
2021-09-06 17:42:04 -0500

    Main menu connect command changes

--- client/DominosPizzaPicker.Client/DominosPizzaPicker.Client/Views/MainMenu.xaml.cs
+++ client/DominosPizzaPicker.Client/DominosPizzaPicker.Client/Views/MainMenu.xaml.cs
@@ -1,14 +1,57 @@
 ﻿using DominosPizzaPicker.Client.Helpers.CustomViews;
+using DominosPizzaPicker.Client.Models.Managers;
 using System;
+using System.Windows.Input;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace DominosPizzaPicker.Client.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MainMenu: CustomMainMenu
+	public partial class MainMenu : CustomMainMenu
 	{
         private async void OnbtnStatsClicked(object sender, EventArgs e)
         {
+
+        }
+
+
+        public Command ConnectCommand { get; private set; }
+
+        public MainMenu() : base()
+        {
+            lblURL.Text = "Current: " + (Constants.ApplicationURL == Constants.LocalIISURL ? "Local IIS URL" : (Constants.ApplicationURL == Constants.ConveyorURL ? "Conveyor URL" : "Not Connected"));
+
+            ConnectCommand = new Command<string>(
+                execute: async (string type) =>
+                {
+                    IsBusy = true;
+                    NavigateCommand.ChangeCanExecute();
+                    ConnectCommand.ChangeCanExecute();
+                    var before = Constants.ApplicationURL;
+                    Constants.ApplicationURL = type.ToUpper().Equals("LOCAL") ? Constants.LocalIISURL : Constants.ConveyorURL;
+                    if (before != Constants.ApplicationURL)
+                    {
+                        lblURL.Text = "Current: ...";
+                        if (await CachedData.ResetConnection())
+                            lblURL.Text = "Current: " + (Constants.ApplicationURL == Constants.LocalIISURL ? "Local IIS URL" : (Constants.ApplicationURL == Constants.ConveyorURL ? "Conveyor URL" : "Not Connected"));
+                        else
+                        {
+                            Constants.ApplicationURL = "NOT CONNECTED";
+                            lblURL.Text = "Current: Not Connected";
+                        }
+                    }
+                    IsBusy = false;
+                    NavigateCommand.ChangeCanExecute();
+                    ConnectCommand.ChangeCanExecute();
+                },
+                canExecute: (string a) =>
+                {
+                    return !IsBusy;
+                });
+
+            BindingContext = null;
+            BindingContext = this;
 
         }
 
